53a9470db192b41a09da73497a087827529ecb7e
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-05-12 16:05:07 +0330

    customize  img slider(3d)

--- app/src/main/java/ir/hoseinahmadi/frenchpastry/ui/screen/home/TopSliderSection.kt
+++ app/src/main/java/ir/hoseinahmadi/frenchpastry/ui/screen/home/TopSliderSection.kt
@@ -21,12 +21,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -35,6 +36,7 @@ import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 import ir.hoseinahmadi.frenchpastry.R
 import kotlinx.coroutines.delay
+import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalPagerApi::class, ExperimentalGlideComposeApi::class)
 @Composable
@@ -55,9 +57,6 @@ fun TopSliderSection(
                 .padding(4.dp)
         ) {
             val pagerState = rememberPagerState()
-            var imageUrl by remember {
-                mutableStateOf("")
-            }
             HorizontalPager(
                 modifier = Modifier
                     .fillMaxWidth(),
@@ -67,13 +66,22 @@ fun TopSliderSection(
                     horizontal = 5.dp,
                 )
             ) { index ->
-                imageUrl = sliderList[index]
+                val pageOffset = (pagerState.currentPage - index) + pagerState.currentPageOffset
+                val scaleFactor = 0.75f + (1f - 0.75f) * (1f - pageOffset.absoluteValue)
                 Box(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
+                        .graphicsLayer {
+                            rotationY = if (pageOffset != 0f) -30 * pageOffset else 0f
+                            scaleX = scaleFactor
+                            scaleY = scaleFactor
+                        }
+                        .alpha(
+                            scaleFactor.coerceIn(0f, 1f)
+                        ),
                     contentAlignment = Alignment.Center
                 ) {
                     GlideImage(
-                        model = imageUrl,
+                        model = sliderList[index],
                         contentDescription = "",
                         modifier = Modifier
                             .padding(5.dp)
@@ -106,7 +114,7 @@ fun TopSliderSection(
                         inactiveColor = Color(0xffD9D9D9),
                         indicatorWidth = 8.dp,
                         indicatorHeight = 8.dp,
-                        indicatorShape = CircleShape
+                        indicatorShape = CircleShape,
                     )
                 }
 
